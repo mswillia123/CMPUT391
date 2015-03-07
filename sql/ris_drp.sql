dd633fc16077e30c48743525207f667f7fc45108
czervos <czervos@ualberta.ca>
2015-03-07 14:52:20 -0700

    Added RIS database schema setup & drop files

---
+++ sql/ris_drp.sql
@@ -0,0 +1,11 @@
+/*
+ *  File name:  ris_drp.sql
+ *  Function:   Drops Radiology Information System database schema
+ *  Author:     Costa Zervos
+ */
+
+DROP TABLE family_doctor;
+DROP TABLE pacs_images;
+DROP TABLE radiology_record;
+DROP TABLE users;
+DROP TABLE persons;
