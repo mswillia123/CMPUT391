fbd453297d1e13515dc14140b3b6deda7875db44
czervos <czervos@ualberta.ca>
2015-03-07 19:23:57 -0700

    Fixed spacing issues with initial files

--- ris/PHPconnectionDB.php
+++ ris/PHPconnectionDB.php
@@ -1,16 +1,17 @@
 <?php
 /*
- *	Function: 	connect()
- *	Purpose:	Connects to the Oracle database using the hardcoded credentials.
- *	Author:		CMPUT 391 Lab 6 PHPconnectionDB.php
+ *	Function:   connect()
+ *	Purpose:    Connects to the Oracle database using the hardcoded credentials.
+ *	Author:     CMPUT 391 Lab 6 PHPconnectionDB.php
  */
 function connect(){
-	$conn = oci_connect('czervos', 'qwerty123');
-	if (!$conn) {
-		$e = oci_error();
-		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-	}
-
-	return $conn;
+    // Enter your OracleDB credentials
+    $conn = oci_connect('user', 'password');
+    if (!$conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+    
+    return $conn;
 }
 ?>
