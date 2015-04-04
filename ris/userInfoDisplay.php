40d2eabfeec5c716e34a3f5817b0a17b4866ddce
czervos <czervos@ualberta.ca>
2015-04-04 13:58:18 -0600

    Added link to help in userInfoDisplay

--- ris/userInfoDisplay.php
+++ ris/userInfoDisplay.php
@@ -1,6 +1,7 @@
 <?php
 /*
- *  Displays the logged in user's name, user type, and a link to logout.
+ *  Displays the logged in user's name, user type, a link to logout, and a link
+ *  to the user documentation.
  *
  *  Author: Costa Zervos
  */
@@ -20,6 +21,8 @@
         echo ' - ';
         // Displays logout link
         echo '<a href="logout.php">Logout</a>';
+        echo ' - ';
+        echo '<a href="https://docs.google.com/document/d/16ihhOwVXXW_k72kDrEZBDumvuH4eqr3jaat82jAxKio/edit?usp=sharing" target="_blank">Help</a>';
         echo'<br/><br/>';
     }
 ?>
